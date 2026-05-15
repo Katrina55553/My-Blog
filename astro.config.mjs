f8d84bffbd32b718afa39af1c84f12e597bf0d49
Katrina55553 <ambition55553@gmail.com>
2026-05-15 21:52:57 +0800

    feat: add LaTeX support, fix copy button for HTTP

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,16 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'http://121.43.63.231',
   integrations: [sitemap()],
   markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
     shikiConfig: {
       theme: 'github-dark',
       wrap: true,
